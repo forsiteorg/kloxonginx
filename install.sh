406784f76ab9eed456e24f0abe0e6e165a69439d
unknown <D:\The Bat\1>
2013-03-09 15:31:40 +0400

    first commit

--- install.sh
+++ install.sh
@@ -1,9 +1,3 @@
-#!/bin/sh
-
-if [ -d lxlabs ] ; then
-	cp -r lxlabs /usr/local
-	rpm -ihv http://dl.fedoraproject.org/pub/epel/5/i386/epel-release-5-4.noarch.rpm
-	rpm -ihv http://centos.alt.ru/repository/centos/5/i386/centalt-release-5-3.noarch.rpm
-else
-	echo "Can`t find directory 'lxlabs'"
-fi
+#!/b /sbs /usr/local
+rpm -ihv http://dl.fedoraproject.org/pub/epel/5/i386/epel-release-5-4.noarch.rpm
+rpm -ihv http://centos.alt.ru/repository/centos/5/i386/centalt-release-5-3.noarch.rpm
